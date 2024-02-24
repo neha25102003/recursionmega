8b3221b51924387f052da430b8fe5837ca604e33
neha25102003 <155766797+neha25102003@users.noreply.github.com>
2024-02-24 22:45:48 +0530

    Add files via upload

---
+++ lengthofstrings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int length(char s[]){
+    if (s[0]==NULL)
+    {
+        return 0;
+    }
+    return 1+length(s+1);
+}
+int main(){
+char str[100];
+cin>>str;
+cout<<length(str);
+}
